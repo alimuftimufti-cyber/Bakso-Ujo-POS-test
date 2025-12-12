2785cc45abc0a81af591f94ba3c879e1295aca32
alimuftimufti-cyber <alimufti.mufti@gmail.com>
2025-12-12 18:11:16 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// FILE INI SUDAH TIDAK DIGUNAKAN (DEPRECATED)
-// Jangan hapus file ini jika menyebabkan error 'File not found', tapi biarkan isinya kosong seperti ini.
-// Entry point aplikasi sekarang adalah /index.tsx
-export {};
+// FILE INI SUDAH TIDAK DIGUNAKAN.
+// Mohon jangan import apapun di sini untuk mencegah error build.
+// Entry point utama adalah index.tsx di root folder.
+export default null;
